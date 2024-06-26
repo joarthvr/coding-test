ca36c59a46a776559834bb44c9731db7ef50f60f
Arthur Jo <ioi456859@gmail.com>
2024-06-27 02:57:19 +0900

    week13

---
+++ week13/체육복.js
@@ -0,0 +1,27 @@
+function solution(n, lost, reserve) {
+    // 여분의 체육복을 가진 학생과 도난당한 학생을 정렬합니다.
+    lost.sort((a, b) => a - b);
+    reserve.sort((a, b) => a - b);
+
+    // 여분의 체육복을 가진 학생이 도난당한 경우를 먼저 처리
+    for (let i = 0; i < reserve.length; i++) {
+        if (lost.includes(reserve[i])) {
+            lost.splice(lost.indexOf(reserve[i]), 1);
+            reserve.splice(i, 1);
+            i--;
+        }
+    }
+    // 도난당한 학생이 여분의 체육복을 빌릴 수 있는지 확인
+    for (let i = 0; i < lost.length; i++) {
+        if(reserve.includes(lost[i] - 1)) {
+            reserve.splice(reserve.indexOf(lost[i] - 1), 1);
+            continue;
+        } 
+        if(reserve.includes(lost[i] + 1)) {
+            reserve.splice(reserve.indexOf(lost[i] + 1), 1);
+            continue;
+        } 
+            n--;
+    }
+    return n;
+}
