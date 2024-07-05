483bd7d46cf700cd2905028c5fc740d033dfcb0c
joarthvr <ioi456859@gmail.com>
2024-07-05 20:20:13 +0900

    week14

---
+++ week14/숫자 문자열과 영단어.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  const alphabet = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  for(let i = 0; i < s.length; i++){
+      alphabet.forEach((e,i) => {
+      if(s.search(e) !== -1){
+          s = s.replace(e,i);
+      }
+  });
+  }
+  
+  return Math.floor(s);
+}
